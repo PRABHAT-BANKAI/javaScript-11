1035bf365c10e2415a7baa956c895f0291bb1420
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-08-22 11:40:03 +0530

    day2

---
+++ day2/script.js
@@ -0,0 +1,24 @@
+console.log("hello world ")
+
+
+// *
+// **
+// ***
+
+
+console.log("*")
+console.log("**")
+console.log("***")
+
+
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+
+console.log("                         All students                ")
+console.log("====================================================")
+console.log( "   P   C    M    ")
