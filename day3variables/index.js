a93a2cc061c663a93b10cc391b84818ea1eccfa8
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-08-25 11:38:04 +0530

    virables

---
+++ day3variables/index.js
@@ -0,0 +1,73 @@
+// what is variables understanding data types declaring and initialization variables
+
+// variables   ( is a container which has name and store the value )
+
+// heroName ,userName, mobileDevice  (camel case ) // user_name , hero_name , mobile_name (snake case)
+
+// let heroName = "Batman";
+
+// console.log(heroName);
+
+// //Data types
+
+// // Primtive  data types
+// // number
+// // string
+// // boolean
+// // undefined
+// // null
+// // big int
+
+// let num1 = 12; // number
+
+// console.log(num1);
+
+// console.log(typeof num1);
+
+// let studentName = "Nistha"; // string  "nistha"  'nistha'  `nistha`
+
+// console.log(studentName);
+// console.log(typeof studentName);
+
+// // boolean (true and false)
+
+// let temp = false; // boolean
+
+// console.log(temp);
+// console.log(typeof temp);
+
+// let employee; //  i cannot initlize the value in employee variable so i will get undefined
+
+// console.log(employee); // undefined get by code
+// console.log(typeof employee);
+
+// // null  ( data type is  object ) // null can assgined  by user
+
+// let age = null;
+
+// console.log(age);
+// console.log(typeof age);
+
+// //BigInt
+// let bigValue = 123456789123456789324324324324n; // bigint
+
+// console.log(bigValue);
+// console.log(typeof bigValue);
+
+// // let value ;// virable declaration
+
+// // value = 132  // value intialization
+// let value = 132; // variable declarated then value will intialzied
+
+// non-primtive data types
+// Array
+// Function
+// Object
+
+let num1 = 15;
+
+num1 = 1
+
+let num2 = 20;
+
+console.log(num1 + num2);
