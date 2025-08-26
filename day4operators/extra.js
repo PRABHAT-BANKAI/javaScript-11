c918ac013ea977a9eeb3767d57775f683d266298
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-08-26 11:48:12 +0530

    operators

---
+++ day4operators/extra.js
@@ -0,0 +1,42 @@
+//increment // decrement
+
+let a = 5;
+
+let value1 = a;
+
+let value2 = --a; // prefix // first increment the value then assigning the value
+
+let value3 = a--; // postfix  // first assinging the value then increment the value
+
+console.log(a); //3
+console.log(value1); //5
+console.log(value2); //4
+console.log(value3); //4
+
+// // Assignment operators
+
+// let sum = 0;
+
+// sum = 4+1
+
+// console.log(sum)
+
+// let x = 10
+
+// let y = 20
+
+// x = y
+
+// console.log(x)
+
+let sum = 0;
+
+// sum = sum + 5;
+
+sum += 5;
+
+sum-=2
+// sum = sum -2
+
+
+console.log("sum is", sum);
---
+++ day4operators/script.js
@@ -0,0 +1,38 @@
+//
+// Types Of Operator
+// Arithmetic operators + - / % **
+// Assignment operators
+// Comparison operators
+// Logical operators
+// conversion with console.log and innerHTML
+
+// Arithmetic operators + - / % **
+
+let a = 20;
+
+let b = 15;
+
+let text = document.getElementById("text");
+
+// text.innerHTML = a+b
+// text.innerHTML = a*b
+
+let result = a - b;
+console.log(result);
+
+// Exponentiation
+
+let c = 5;
+
+console.log(c ** 3);
+
+// divison
+let d = 11;
+
+console.log(d / 2); // return quetient 5.5
+
+// 	Modulus (Remainder) %
+
+let e = 14;
+
+console.log(e % 2);
