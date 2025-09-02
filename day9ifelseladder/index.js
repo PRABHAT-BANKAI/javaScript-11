89056de7ffc23aa4dc3bf08793e8fc2c6f9538c9
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-09-02 11:42:55 +0530

    ifelseladder

---
+++ day9ifelseladder/index.js
@@ -0,0 +1,117 @@
+// 	if else ladder Statement Choice
+
+// if(){
+
+// }
+
+// if(){
+
+// }else{
+
+// }
+
+// if(){
+//   if(){
+
+//   }
+// }else{
+
+// }
+
+// if else ladder syntax
+// if(condition1){
+
+// }else if( condition2){
+
+// }else if(condtion3){
+
+// }else{
+
+// }
+
+// let light = "blue";
+
+// if (light === "red") {
+//   console.log("stop");
+// } else if (light === "yellow") {
+//   console.log("slow down");
+// } else if (light === "green") {
+//   console.log("go");
+// } else {
+//   console.log("invalid color");
+// }
+
+// let score = 99;
+
+// if (score >= 95) {
+//   console.log("Grade A+");
+// } else if (score >= 80) {
+//   console.log("Grade A");
+// } else if (score >= 70) {
+//   console.log("Grade B+");
+// } else if (score >= 60) {
+//   console.log("Grade B");
+// } else if (score >= 50) {
+//   console.log("Grade C");
+// } else if (score >= 40) {
+//   console.log("Grade D");
+// } else {
+//   console.log("fail");
+// }
+
+let budget = 5000;
+
+if (budget >= 10000) {
+  console.log("dwarikadish");
+} else if (budget >= 8000) {
+  console.log("rajkot");
+} else if (budget >= 6000) {
+  console.log("polo forest");
+} else if (budget >= 4000) {
+  console.log("dominoz");
+} else if (budget >= 2000) {
+  console.log("local restaurent");
+} else if (budget >= 500) {
+  console.log("vada pao");
+} else {
+  console.log("ghar betho");
+}
+
+let age = 50;
+
+if (age >= 60 && age <= 100) {
+  console.log("senior citizen");
+} else if (age >= 40 && age < 60) {
+  console.log("aged");
+} else if (age >= 21 && age < 40) {
+  console.log("younger");
+} else if (age >= 13 && age < 21) {
+  console.log("teenagers");
+} else {
+  console.log("children");
+}
+
+
+
+
+// revision grade
+
+
+let score = 75
+
+
+
+if(score > 90){
+  console.log("Grade A")
+}else{
+  if(score >=80 && score <=89 ){
+    console.log("Grade B")
+  }else{
+    if(score >=70 && score <= 79){
+      console.log("Grade c")
+    }else{
+      
+    }
+  }
+}
+
