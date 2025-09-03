20b6524c08e7ff06fb39f801e066c3cc1808f96b
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-09-03 11:46:17 +0530

    revision

---
+++ day10/index.js
@@ -0,0 +1,75 @@
+// // primitive
+// // undefined
+// // null
+// // string
+// // number
+// // Boolean
+// // Bigint
+
+// // non primitive (reference store )
+// // Object
+// // Array  // store  / collection of multiple data types
+// // functions
+
+// // let arr = [1, 2, 3, 4, "hello", true];// reference  a2#@a
+
+// // let arr2 = [ 1,2] //reference @3242
+
+// // let arr = [1,2,3]// #@#@GSA
+// // let arr2 = arr//  #@#@GSA
+
+// // console.log(arr == arr2)// false
+// // //RAM (random access memory)
+
+// // const number = 12 // const variable ko constant rakhta hai
+
+// // number = 15// cannot update again dynamically  with const keyword
+
+// // dynamically in JS
+
+// // let  value = 12
+
+// // value = true
+
+// // value = "string" // dynamically change
+
+// // console.log(number)
+
+// // let number = 12
+// // number = 15
+
+// // let number = 17
+
+// var number = 15 //  is to old  // features let and const
+
+// number = 19
+
+// var number = 35
+
+// // 1000 thousand lines code
+
+// let  userData = "store"
+
+// //after 1000 lines
+//  var userData2 = "second store data"
+
+//  console.log(userData)
+
+//  variable == declaration // when value assining variable // intialization
+
+// implicit
+
+console.log("15" - 5);
+
+console.log(15 + "15" + +"15");
+
+console.log("hi" + " " + "from" + " " + "javascript");
+
+console.log(false <= true - 1); //0 <= 0
+
+console.log("" == 0); // false == 0  // 0 == 0  // empty string = ""
+
+let str = ""; // empty string
+
+let result = Boolean(str);
+console.log(result);
