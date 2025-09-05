59a8728764d2c0f3d27b6be1803c74496e2b1e7a
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-09-05 11:44:50 +0530

    for loop and while loop

---
+++ day11/index.js
@@ -0,0 +1,71 @@
+// loop
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+
+// 1 to 10000
+
+// // entry loop
+// while
+// for
+
+// // exit loop
+// do while
+
+// while loop
+
+// value inintialzaiton
+
+// while(conditon){
+
+//   // increment decrement i++ i--
+// }
+
+// let i = 1; // 1. intialization
+
+// while (i <= 100) { // conditon
+//   //10001<=10000//false// when loop condition it will break loop sae bahar ajayga
+//   console.log(i); //1//2//3//4//9999/10000//execute
+//   i++; //2//3//4//5 //10001// inc dec
+// }
+
+// let i = 100;
+
+// while (i >= 1) {
+//   //0>=1(false)
+// if(i%2==0){
+//   console.log(`even number is ${i}`) //1
+// }
+//   i--; //0
+// }
+
+// console.log("end");
+
+// for loop
+
+// for(intialization;condition;inc/dec){// block
+
+// }
+
+// dry run
+// 1. intialization i=1
+// 2. condtion check i<=100
+//3. execution in block
+//4. inc/dec
+
+// for (let i = 1; i <= 100; i++) {
+//   console.log(i);//1//2//3
+// }
+
+let i = 5; // i=5
+
+while (i > 1) {// 5>=1(true)//4>=1(true)//3>=1(true)//2>=1(true)//1>=1(true)//0>=1(false)
+  console.log(i);// 5// 4//3////2//1
+  i--;//4//3//2//1//0
+}// when my codition false so will break loop and my code come to the next line 
+
+console.log("end")
