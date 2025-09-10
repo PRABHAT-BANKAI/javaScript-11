43927a11674ece85489bc4a33d44b3bbfd1192b4
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-09-10 11:48:04 +0530

    revision2

---
+++ revision/index.js
@@ -0,0 +1,19 @@
+let number = 15;
+
+if (number % 2 == 0) {
+  // true //false
+
+  console.log(`my number is ${number} even `);
+} else {
+  // when conditon if is in  false  condition
+  console.log(`my number is ${number} odd `);
+}
+
+let value = -15;
+
+// 5>=5  4>=5
+if (value >= 0 && value <= 50 && value % 5 == 0) {
+  console.log("yes my number is between 0 to 50 and divisible by 5");
+} else {
+  console.log("my number is not between 0 to 50 and not divisible by 5  ");
+}
---
+++ revision/index2.js
@@ -0,0 +1,32 @@
+// // let number = 5;
+
+// // for (let i = 1; i <= number; i++) { //i=2 //2<=5
+// //   let str = ""; // ""
+// //   for (let j = 1; j <= i; j++) {//j=1// 1<=2
+// //     str = str + i   // "" + 2  // "2"+2
+
+// //   }
+// //       console.log(str)  // 1
+// // }
+
+// // let number = 5
+// // let str=""
+// // for(let i=1;i<=number;i++){
+// // str = str + i //"1"+2
+// // console.log(str)//"12"
+// // }
+//  let str = 0;//globally created
+// for (let i = 1; i <= 5; i++) {//
+
+//   str +=i  //0+1 =1+2=3+3=6+4=10+5=15
+
+// }
+
+//  console.log(str);//"12345"
+
+
+ for(let i=1;i<=5;i++){//i=1,i=2,i=3,i=4,i=5
+  for(let j=1;j<=2;j++){//2 bar//2 bar// 2bar//2bar//2bar
+   console.log("hi"+j)
+  }
+ }
