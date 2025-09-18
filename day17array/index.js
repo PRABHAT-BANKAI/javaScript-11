34aa9c2cac8c14fd772e6af73c04928951b3d7b8
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-09-18 11:44:49 +0530

    array

---
+++ day17array/index.js
@@ -0,0 +1,42 @@
+// //Introduction of Array in JS (non primitive data type)(carry the reference )
+
+// //Array
+
+// //syntax array=[]// so i  can store multiple of data types in array
+
+// let store = [1, 2, 3, 4, 5, 55, 44, 98]; // elements
+// // index     0 ,1, 2, 3 ,4, 5 6
+
+// console.log(store[2]);
+
+// console.log(store[0]);
+// console.log(store[4]);
+// console.log(store[20]);
+
+// console.log(store.length);
+
+// //loop use kr skta hu
+// //store.length-1 = for getting last index of an array
+
+let store = [1, 2, 3, 4, 5, 55, 44, 98];
+
+// for (let i = 0; i <= store.length - 1; i++) {
+//   // console.log(store[i]);
+//   if (store[i] % 2 == 0) {
+//     console.log(store[i]);
+//   }
+// }
+
+// for (let i = store.length - 1; i >= 0; i--) {
+//   console.log(store[i]);
+// }
+
+// let  array = [1,"12",true,null,123]
+
+// const years = [12, 12, 5, 12];
+
+// console.log(years["2"] !== years["02"]);// 5 !== undefined 
+
+// console.log(years["2"]);
+
+// console.log(years["02"]);
