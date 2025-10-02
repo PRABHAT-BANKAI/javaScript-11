cb6a28b68670c73124f11e439b9aec43ac034ab1
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-10-02 11:48:14 +0530

    revisionifelsefunction

---
+++ day24revision/function.js
@@ -0,0 +1,39 @@
+// // normal function
+// // anonymous function
+// // variable function
+
+// // show()
+
+// // function show(){// block of code
+
+// //   console.log("hello world")
+// // }
+
+// // show()
+// // show()
+
+// function sum(a,b){//parameters
+// console.log(a+b)
+// }
+
+// sum(15,2)//arguments
+
+// sum("1","2")// concatenate
+
+// sum(1,2)
+
+// // console.log(1+1+"7")//left to right
+
+let email = "abcd@gmail.com";
+let password = "12345";
+
+if (email == "abcd@gmail.com") {
+
+    if (password == "12345") {
+    console.log("login successfully");
+  } else {
+    console.log("invalid password");
+  }
+} else {
+  console.log("invalid credentials");
+}
---
+++ day24revision/index.js
@@ -0,0 +1,25 @@
+// 1
+// 12
+// 123
+// 12345
+
+//12345 // string because of concatenate
+//12345
+//12345
+//12345
+//12345
+
+//1
+//2
+//3
+//4
+//5
+
+
+for (let i = 1; i <= 5; i++) { // help to create row
+  let str = "";
+  for (let j = 1; j <= i; j++) {
+    str += j
+  }
+  console.log(str)
+}
