eb74e4c7978538dd14775945ccae114e57806efc
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-10-03 11:49:10 +0530

    revision25

---
+++ day25revisionfunc/index.js
@@ -0,0 +1,116 @@
+// // function sum(a, b) {
+
+// //   // run
+// //   return a + b;
+
+// //   ///
+// // }
+
+// // let ajay = sum(12, 4);
+
+// // let raj = sum(14, 6);
+
+// // console.log(ajay);
+// // console.log(raj);
+
+// for (let i = 1; i <= 5; i++) {//i=2  
+//   let str = "";  //""
+//   for (let j = 1; j <= 5; j++) {
+//     str += j;  // "12345"
+//   }
+
+//   console.log(str);
+
+// }
+
+// //"12345"
+// //"12345"
+// //"12345"
+// //"12345"
+// //"12345"
+
+
+// for (let i = 1; i <= 5; i++) {//i=1
+//   let str = "";//""
+//   for (let j = 1; j <= 5; j++) {
+//     str += j;//"1"+2
+//     //"12"
+//     console.log(str);
+//   }
+// }
+
+// //1
+// //12
+
+
+
+let string = ""
+for(k=1;k<=5;k++){//2<=5
+string += k//12
+
+}
+console.log(string)
+
+//1
+//12
+//123
+//1234
+//12345
+
+//1
+//12
+//123
+//1234
+//12345
+
+
+//1
+//12
+//123
+//1234
+//12345
+
+
+//1
+//12
+//123
+//1234
+//12345
+
+
+//1
+//12
+//123
+//1234
+//12345
+
+
+console.log(1)
+
+
+
+console.log(15%2)
+
+
+//arrow function 
+
+const subract = (a,b)=>{
+  return a-b
+}
+
+// console.log(subract(12,9))
+// let result = subract(12,9)
+// console.log(result)
+
+console.log(subract(12,9))
+
+
+
+
+
+
+
+
+
+
+
