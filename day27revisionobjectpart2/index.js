a692169e73a4237e722edb2652c5ae949736319c
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-10-10 11:49:40 +0530

    revison

---
+++ day27revisionobjectpart2/index.js
@@ -0,0 +1,44 @@
+// let student = {
+//   studentName: "batman",
+//   class: "12th",
+//   show: function (hero,age) {
+//     console.log(`hello ${hero} from abcd school  age= ${age}`);
+//   },
+// };
+
+// // console.log(student.show)
+
+// console.log(student)
+
+// student.show("batman",15)
+// student.show("superman",17)
+
+let total = 0;
+for (let i = 1; i <= 100; i++) {
+
+  if (i % 2 == 0) {
+    total += i;
+    
+  }
+ 
+}
+  console.log(total);
+
+
+
+
+// function validation(userId, password) {
+//   if (userId === "abcd") {
+//     if (password === 12345) {
+//       console.log("login");
+//     } else {
+//       console.log("password is wrong");
+//     }
+//   } else {
+//     console.log("wrong credential  ");
+//   }
+// }
+
+// validation("abcd", 12345);
+
+// validation("abcd",12345)
