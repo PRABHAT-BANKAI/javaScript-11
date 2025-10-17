bafa1e233d24d78ae226ca6e71c122a3521b1fb7
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-10-17 11:54:49 +0530

    revison

---
+++ day32revisonmethodsarrayandstring/index.js
@@ -0,0 +1,50 @@
+//array methods
+
+// Array With Functions Push ,Pop, shift, upshifts, Splice string function
+
+//push //
+//New elements to add to the array.
+//Appends new elements to the end of an array, and returns the new length of the array.
+// let array = [1,2]
+
+// array.push(4)
+
+// console.log(array)
+
+// let emptyArray = []
+// for(let i=1;i<=8;i++){
+// emptyArray.push(i)
+// }
+// console.log(emptyArray)
+
+//pop
+//Removes the last element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+// let array = [1, 2, 3];
+
+// array.pop();
+// console.log(array);
+
+// let arrStore = [1,2,3,4,5,6,7,8]
+
+// for(let i=3;i>=0;i--){
+//   arrStore.pop()
+// }
+// console.log(arrStore)
+
+
+let array = [1,2,3]
+
+// upshift
+//shift
+
+array.unshift()
+//Elements to insert at the start of the array.
+//Inserts new elements at the start of an array, and returns the new length of the array.
+// 6
+
+
+
+
+
+
+
